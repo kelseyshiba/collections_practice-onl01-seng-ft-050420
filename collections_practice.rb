7f6a7b768c7a07a5d3ece2d3935f0c7b8a58969a
Kelsey Shiba <kelsey.shiba@gmail.com>
2020-05-05 22:34:18 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,25 @@
+def sort_array_asc(array)
+  array.sort
+end
 
+def sort_array_desc(array)
+  array.sort do |a, b|
+    b <=> a
+  end
+end
+
+def sort_array_char_count(array)
+  array.sort do |word, length|
+    word.length <=> length.length
+  end
+end      
+
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+  array
+end
+
+def reverse_array(array)
+  array.reverse
+  array
+end
